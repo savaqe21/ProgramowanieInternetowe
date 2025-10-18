34fa1a781010b1466c84ea2acc4ea3d0d1593373
savaqe21 <fabianlesniak@icloud.com>
2025-10-18 21:27:01 +0200

    Finishing project

--- witaj/urls.py
+++ witaj/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('hello/', views.hello, name='hello'),
     path('hello/<str:name>/', views.hello_name, name='hello_name'),
     path('hello_template/<str:name>/', views.hello_template, name='hello_template'),
--- witaj/views.py
+++ witaj/views.py
@@ -2,32 +2,21 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from datetime import datetime
 
+def index(request):
+    return HttpResponse("Witaj na stronie głównej!<br>Przejdź do /hello/ lub /time/")
 
 def hello(request):
-    """Podstawowy widok powitalny"""
     return HttpResponse("Witaj w Django!")
 
-
 def hello_name(request, name):
-    """Dynamiczny widok powitalny z parametrem name"""
     return HttpResponse(f"Witaj, {name}!")
 
-
 def hello_template(request, name):
-    """Widok powitalny używający szablonu HTML"""
     return render(request, "witaj/hello.html", {"name": name})
 
-
 def current_time(request):
-    """Widok wyświetlający aktualną datę i godzinę - ZADANIE DOMOWE"""
     now = datetime.now()
-    # Format: Dzisiaj jest: 22.09.2025, godzina: 18:42
     formatted_date = now.strftime("%d.%m.%Y")
     formatted_time = now.strftime("%H:%M")
-
-    context = {
-        'date': formatted_date,
-        'time': formatted_time
-    }
-
+    context = {'date': formatted_date, 'time': formatted_time}
     return render(request, "witaj/time.html", context)
